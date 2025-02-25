5b242301970fe55b6ead4ca5918836eae6a709fe
wehttqm <matthewfalcone5@gmail.com>
2025-02-24 23:05:13 -0500

    Update base path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/pathway/",
+  base: "https://wehttqm.xyz/pathway/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
