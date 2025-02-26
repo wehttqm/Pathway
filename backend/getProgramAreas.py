248df6406bd1f50b2dad7964442e3566a360bd28
wehttqm <matthewfalcone5@gmail.com>
2025-02-25 23:52:02 -0500

    Add program area scraper

---
+++ backend/getProgramAreas.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from prereqParser import hunt_for_prereqs
+import traceback
+
+def _get_program_areas(html: bytes):
+    soup = BeautifulSoup(html, 'html.parser')
+    div = soup.find('div', class_=re.compile('^w3-row node__content'))
+    inner_div = div.find('div', class_=re.compile('w3-row field field--name-body field--type-text-with-summary field--label-hidden w3-bar-item field__item'))
+    tables = inner_div.find_all('table', recursive=False)
+
+    program_areas = []
+    _names = []
+
+    for table in tables:
+        body = table.find('tbody')
+        rows = body.find_all('tr')
+        for row in rows:
+            d = row.find('td')
+            a = d.find('a')
+
+            text = a.get_text(strip=False)
+            # prevent duplicates
+            if text not in _names:
+                program_areas.append({"href": a['href'], "name": text})
+                _names.append(text)
+
+    return program_areas
+
+
+def get_program_areas(url: str = "https://utm.calendar.utoronto.ca/list-program-areas"): 
+    response = requests.get(url) 
+
+    if response.status_code != 200:
+        print("Error getting HTML")
+    else:
+        html = response.content
+        return _get_program_areas(html)
+    
+
+if __name__ == "__main__":
+    print(get_program_areas())
+
