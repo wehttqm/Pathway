a6f0b11dbdc3a35f6195cdc02e1c9934f18be1bd
wehttqm <matthewfalcone5@gmail.com>
2025-03-23 14:18:14 -0400

    lint

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,31 +1,44 @@
 import eslint from "@eslint/js";
+
 import eslintPluginPrettier from "eslint-plugin-prettier";
+
 import tseslint from "typescript-eslint";
+
 import prettierConfig from "eslint-config-prettier";
+
 import react from "eslint-plugin-react";
 
 export default tseslint.config(
   {
     ignores: ["src/components/ui/"],
   },
+
   {
     ...react.configs.flat.recommended,
+
     settings: {
       react: {
         version: "detect",
       },
     },
   },
+
   eslint.configs.recommended,
+
   tseslint.configs.recommended,
+
   prettierConfig,
+
   {
     plugins: { prettier: eslintPluginPrettier },
+
     rules: eslintPluginPrettier.configs.recommended.rules,
   },
+
   {
     rules: {
       "react/react-in-jsx-scope": "off", // Disable React-in-JSX-scope rule,
+
       "@typescript-eslint/no-require-imports": "off",
     },
   },
